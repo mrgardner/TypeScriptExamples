09a0c7388a57948a3bf6744a3435f8ca31ed02f6
Matthew Gardner <mgardner@kenzanmedia.com>
2016-09-29 16:01:44 -0400

    Finished compiler exercises

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("It works");
+var myName = 'Matt';
+var myAge = 22;
+var anything;
+anything = 12;
+function controlMe(isTrue, somthingElse) {
+    var result;
+    if (isTrue) {
+        result = 12;
+    }
+    result = 33;
+    return result;
+}
--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log("It works");
+let myName: string = 'Matt';
+let myAge : number = 22;
+let anything;
+anything = 12;
+
+function controlMe(isTrue: boolean, somthingElse: boolean) {
+    let result: number;
+    if (isTrue) {
+        result = 12;
+    }
+    result = 33;
+    return result;
+}
