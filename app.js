46c8423733ada106a58e3ee42f0c1170156c485d
Matthew Gardner <mgardner@kenzanmedia.com>
2016-09-29 12:58:36 -0400

    Adding files for basic examples

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("It works");
+// string
+var myName = 'Matt';
+// myName = 28; Error
+// number
+var myAge = 22;
+// myAge = 'Matt'; Error
+// boolean
+var hasHobbies = true;
+// hasHobbies = 1; Error
+// assign types
+var myRealAge;
+myRealAge = 27;
+// myRealAge = '27';  Error
+// array
+var hobbies = ["Cooking", "Sports"];
+hobbies = [100];
+// hobbies = 100 Error
+// tuples
+var address = ["Superstreet", 99];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 2] = "Blue"; // 2
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
+// any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
--- app.ts
+++ app.ts
@@ -1,1 +1,40 @@
-console.log("It works");
+// string
+let myName: string = 'Matt';
+// myName = 28; Error
+
+// number
+let myAge: number = 22;
+// myAge = 'Matt'; Error
+
+// boolean
+let hasHobbies: boolean = true;
+// hasHobbies = 1; Error
+
+// assign types
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = '27';  Error
+
+// array
+let hobbies: any[] = ["Cooking", "Sports"];
+hobbies = [100];
+// hobbies = 100 Error
+
+// tuples
+let address: [string,number] = ["Superstreet", 99];
+
+// enum
+enum Color {
+    Gray, // 0
+    Green = 100, // 1
+    Blue = 2 // 2
+}
+
+let myColor: Color = Color.Green;
+console.log(myColor);
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = {brand: "BMW", series: 3};
+console.log(car);
