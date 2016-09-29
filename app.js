3cfca28efab92f982ecedf83b1e8c2e3588a1a32
Matthew Gardner <mgardner@kenzanmedia.com>
2016-09-29 17:10:02 -0400

    Finished es6 section

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
-console.log("It works");
+// let & const
+console.log("LET & CONST");
+var variable = "Test";
+console.log(variable);
+variable = "Another value";
+console.log(variable);
+var maxLevels = 100;
+console.log(maxLevels);
+// maxLevels = 99;  Won't Work
+// Block scope
+function reset() {
+    var variable = null;
+    console.log(variable);
+}
+reset();
+console.log(variable);
+// Arrow Functions
+console.log("ARROW FUNCTIONS");
+var addNumbers = function (number1, number2) {
+    return number1 + number2;
+};
+console.log(addNumbers(10, 3));
+var multiplyNumbers = function (number1, number2) { return number1 * number2; };
+console.log(multiplyNumbers(10, 3));
+var greet = function () {
+    console.log("Hello!");
+};
+greet();
+var greetFriend = function (friend) { return console.log(friend); };
+greetFriend("Manu");
+//Default Parameters
+console.log("DEFAULT PARAMETERS");
+var countdown = function (start) {
+    if (start === void 0) { start = 10; }
+    while (start > 0) {
+        start--;
+    }
+    console.log("Done!", start);
+};
+countdown();
+// Rest & Spread
+console.log("REST & SPREAD");
+var numbers = [1, 10, 99, -5];
+console.log(Math.max(33, 99, 10, -3));
+console.log(Math.max.apply(Math, numbers));
+function makeArray() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i - 0] = arguments[_i];
+    }
+    return args;
+}
+console.log(makeArray(1, 2, 6));
+// Destructuring
+console.log("DESTRUCTURING");
+var myHobbies = ["Cooking", "Sports"];
+var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
+console.log(hobby1, hobby2);
+var userData = { userName: "Matt", age: 22 };
+var myName = userData.userName, myAge = userData.age;
+console.log(myName, myAge);
+// Template Literals
+console.log("TEMPLATE LITERALS");
+var userName = "Matt";
+var greeting = "This is a heading!\nI'm " + userName + ".\nThis is cool!";
+console.log(greeting);
--- app.ts
+++ app.ts
@@ -1,1 +1,77 @@
-console.log("It works");
+// let & const
+console.log("LET & CONST");
+let variable = "Test";
+console.log(variable);
+variable = "Another value";
+console.log(variable);
+
+const maxLevels = 100;
+console.log(maxLevels);
+// maxLevels = 99;  Won't Work
+
+// Block scope
+function reset() {
+    let variable = null;
+    console.log(variable);
+}
+reset();
+console.log(variable);
+
+// Arrow Functions
+console.log("ARROW FUNCTIONS");
+const addNumbers = function (number1: number, number2:number):number {
+    return number1 + number2;
+};
+console.log(addNumbers(10,3));
+
+const multiplyNumbers = (number1: number, number2: number) => number1 * number2;
+console.log(multiplyNumbers(10,3));
+
+const greet = () => {
+    console.log("Hello!")
+};
+greet();
+
+const greetFriend = friend => console.log(friend);
+
+greetFriend("Manu");
+
+//Default Parameters
+console.log("DEFAULT PARAMETERS");
+const countdown = (start: number = 10):void => {
+    while (start > 0) {
+        start--;
+    }
+    console.log("Done!", start);
+};
+countdown();
+
+// Rest & Spread
+console.log("REST & SPREAD");
+const numbers = [1, 10, 99, -5];
+console.log(Math.max(33,99,10,-3));
+console.log(Math.max(...numbers));
+
+function makeArray(...args: number[]) {
+    return args;
+}
+console.log(makeArray(1,2,6));
+
+// Destructuring
+console.log("DESTRUCTURING");
+const myHobbies = ["Cooking", "Sports"];
+const [hobby1, hobby2] = myHobbies;
+console.log(hobby1, hobby2);
+
+const userData = {userName: "Matt", age: 22};
+const {userName: myName, age: myAge} = userData;
+console.log(myName, myAge);
+
+// Template Literals
+console.log("TEMPLATE LITERALS");
+const userName = "Matt";
+const greeting = `This is a heading!
+I'm ${userName}.
+This is cool!`;
+console.log(greeting);
+
