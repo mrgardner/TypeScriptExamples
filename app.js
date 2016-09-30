b6af865298acdb8d97636cdc3e21be4f5be09db9
Matthew Gardner <mgardner@kenzanmedia.com>
2016-09-30 14:50:44 -0400

    Finished interfaces

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-console.log("It works");
+function greet(person) {
+    console.log("Hello, " + person.firstName);
+}
+function changeName(person) {
+    person.firstName = "Anna";
+}
+var person = {
+    firstName: "Matt",
+    hobbies: ["Cooking", "Sports"],
+    age: 22,
+    greet: function (lastName) {
+        console.log("Hi, I am " + this.firstName + " " + lastName);
+    }
+};
+// greet({firstName: "Matt", age: 22});
+changeName(person);
+greet(person);
+person.greet("Anything");
+var Person = (function () {
+    function Person() {
+    }
+    Person.prototype.greet = function (lastName) {
+        console.log("Hi, I am " + this.firstName + " " + lastName);
+    };
+    return Person;
+}());
+var myPerson = new Person();
+myPerson.firstName = "Matt";
+myPerson.lastName = "Anything";
+greet(myPerson);
+myPerson.greet(myPerson.lastName);
+var myDoubleFunction;
+myDoubleFunction = function (value1, value2) {
+    return (value1 + value2) * 2;
+};
+console.log(myDoubleFunction(10, 20));
+var oldPerson = {
+    age: 27,
+    firstName: "Matt",
+    greet: function (lastName) {
+        console.log("Hello!");
+    }
+};
+console.log(oldPerson);
--- app.ts
+++ app.ts
@@ -1,1 +1,73 @@
-console.log("It works");
+interface NamedPerson {
+    firstName: string;
+    age?: number;
+    [propName: string]: any;
+    greet(lastName: string): void;
+}
+
+function greet(person: NamedPerson) {
+    console.log("Hello, "+ person.firstName);
+}
+
+function changeName(person: NamedPerson) {
+    person.firstName = "Anna";
+}
+
+const person: NamedPerson = {
+    firstName: "Matt",
+    hobbies: ["Cooking", "Sports"],
+    age: 22,
+    greet(lastName: string) {
+        console.log("Hi, I am "+ this.firstName + " " + lastName);
+    }
+};
+
+// greet({firstName: "Matt", age: 22});
+changeName(person);
+greet(person);
+person.greet("Anything");
+
+class Person implements NamedPerson {
+    firstName: string;
+    lastName: string;
+    greet(lastName: string) {
+        console.log("Hi, I am "+ this.firstName + " " + lastName);
+    }
+}
+
+const myPerson = new Person();
+myPerson.firstName = "Matt";
+myPerson.lastName = "Anything";
+greet(myPerson);
+myPerson.greet(myPerson.lastName);
+
+// Function Types
+
+
+interface DoubleValueFunc {
+    (number1: number, number2: number): number;
+}
+
+let myDoubleFunction: DoubleValueFunc;
+myDoubleFunction = function (value1: number, value2: number) {
+    return (value1 + value2) * 2
+};
+
+console.log(myDoubleFunction(10,20));
+
+
+// Interface Inheritance
+
+interface AgedPerson extends NamedPerson {
+    age: number
+}
+
+const oldPerson: AgedPerson = {
+    age: 27,
+    firstName: "Matt",
+    greet(lastName: string) {
+        console.log("Hello!");
+    }
+};
+
+console.log(oldPerson);
